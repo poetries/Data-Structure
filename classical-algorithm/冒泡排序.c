0abeee44ae78eef2c32ffc0161a96318c3b98c5c
Poetry <jingguanliuye@gmail.com>
2016-04-26 22:16:28 +0800

    Create 冒泡排序.c

---
+++ classical-algorithm/冒泡排序.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void sort(int *a,int len)
+{
+    int i,j,t;
+    
+    for( i = 0;i<len-1;++i)
+    {
+        for(j = 0;j<len-1-i;++j)
+        {
+            if(a[j] >a[j+1])
+            {
+                t  = a[j];
+                a[j] = a[j+1];
+                a[j+1] = t;
+            }
+        }
+    }
+        
+}
+void main()
+{
+    int a[6] = {10,2,8,-8,11,0};
+    int i = 0;
+    sort(a,6);
+    
+    for(i = 0; i<6;++i)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+}
