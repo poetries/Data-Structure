fb03986579ec013ec7da401cd7f0a4cb4fe5dab5
Poetry <jingguanliuye@gmail.com>
2016-04-27 19:57:17 +0800

    Create Array_Point_1.cpp

---
+++ Haobin_Data-Structure/otherVersion/1-5/Array_Point_1.cpp
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main(void)
+{
+	int a[5] = {1,2,3,4,5};
+
+	//a[3] == *(3+a);
+
+	printf("%p\n", a+1);
+	printf("%p\n", a+2);
+	printf("%d\n", *a+3); //*a+3等价于 a[0]+3
+	
+
+
+
+	return 0;
+}
